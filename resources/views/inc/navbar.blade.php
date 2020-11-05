daa081de5149b49b548bf9f47e29fa0be4098abb
3akram <m.akram2122@gmail.com>
2020-11-05 18:08:51 +0200

    fix(views): remove filters link

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -39,9 +39,7 @@
                             <a class="dropdown-item" href="/dashboard">
                                 Dashboard
                             </a>
-                            <a class="dropdown-item" href="/filter">
-                                filters
-                            </a>
+                            
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
